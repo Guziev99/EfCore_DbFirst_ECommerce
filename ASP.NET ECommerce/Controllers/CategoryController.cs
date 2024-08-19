a646cd90318e9b0dcf382312746d4370827b7abe
User <mahamguziyev@gmail.com>
2024-08-20 02:04:02 +0400

    first

---
+++ ASP.NET ECommerce/Controllers/CategoryController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASP.NET_ECommerce.Domain.Entities.Concretes;
+using ASP.NET_ECommerce.DataAccess.Reposiotries.Abstracts;
+
+namespace ASP.NET_ECommerce.MVC.Controllers;
+
+public class CategoryController : Controller {
+
+    private readonly ITagRepository _TagRepository;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CategoryController(ICategoryRepository categoryRepository, ITagRepository tagRepository) {
+        _categoryRepository = categoryRepository;
+        _TagRepository = tagRepository;
+    }
+
+    [HttpGet]
+    public IActionResult AddCategory() {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddCategory(Category category) {
+        await _categoryRepository.AddAsync(category);
+        return View();
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllCategory() {
+        var categories = await _categoryRepository.GetAllAsync();
+        return View(categories);
+    }
+
+    public async Task<IActionResult> AllTags() {
+        var categories = await _categoryRepository.GetAllAsync();
+        var categoryTags = new List<CategoryTag>();
+        foreach(var category in categories) {
+            var categoryTag = new CategoryTag();
+            categoryTag.Tags = new List<Tag>();
+            categoryTag.Category = category;
+            foreach(var tag in category.Tags) {
+                categoryTag.Tags.Add(tag);
+            }
+            if (categoryTag.Tags.Count != 0) categoryTags.Add(categoryTag);
+        }
+        return View(categoryTags);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> AddTag() {
+        return View(new CategoryTag());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddTag(CategoryTag categoryTag) {
+         var category = await _categoryRepository.GetByIdAsync(categoryTag.CategoryId);
+        category.Tags.Add(await _TagRepository.GetByIdAsync(categoryTag.TagId));
+        var tag = await _TagRepository.GetByIdAsync(categoryTag.TagId);
+        tag.Categories.Add(await _categoryRepository.GetByIdAsync(categoryTag.CategoryId));
+        _categoryRepository.Update(category);
+        _TagRepository.Update(tag);
+        return View(categoryTag);
+    }
+}
---
+++ ASP.NET ECommerce/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASP.NET_ECommerce.Domain.Entities.Concretes;
+using ASP.NET_ECommerce.DataAccess.Reposiotries.Abstracts;
+
+namespace ASP.NET_ECommerce.MVC.Controllers {
+    public class ProductController : Controller {
+
+        private readonly ITagRepository _TagRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ProductController(ICategoryRepository categoryRepository, IProductRepository productRepository, ITagRepository tagRepository) {
+            _categoryRepository = categoryRepository;
+            _productRepository = productRepository;
+            _TagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddProduct() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(Product product) {
+            var category = await _categoryRepository.GetByIdAsync(product.CategoryId);
+            product.Category = category;
+            _productRepository.AddAsync(product);
+            category.Products.Add(product);
+            _categoryRepository.Update(category);
+            return View(product);
+        }
+
+        public async Task<IActionResult> GetAllProducts() {
+            return View(await _productRepository.GetAllAsync());
+        }
+
+        public async Task<IActionResult> AllTags() {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddTag(int id) {
+            var product = await _productRepository.GetByIdAsync(id);
+            ViewBag.Tags = await _TagRepository.GetAllAsync();
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddTag(int id, int[] tags) {
+            var product = await _productRepository.GetByidWithTags(id);
+
+            foreach (var tagId in tags) {
+                var tag = await _TagRepository.GetByIdAsync(tagId);
+                product.Tags.Add(tag);
+            }
+
+            await _productRepository.SaveChanges();
+            return RedirectToAction("AddTag");
+        }
+    }
+}
